674e47d3620f8e733c7c95f5f10696738c7273d6
sigmotoa <si.galvismotoa@gmail.com>
2025-05-15 08:03:19 -0500

    Adding a HTML response

--- app/routes/home.py
+++ app/routes/home.py
@@ -8,3 +8,18 @@ router = APIRouter()
 @router.get("/", response_class=HTMLResponse)
 async def read_home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
+
+
+@router.get("/html", response_class=HTMLResponse)
+async def pure_html():
+    html_content = """
+        <html>
+            <head>
+                <title>sigmotoa</title>
+            </head>
+            <body>
+                <h1>Look ma! HTML!</h1>
+            </body>
+        </html>
+        """
+    return HTMLResponse(content=html_content, status_code=200)
