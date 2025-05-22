446c28a8c5d877600863a2cb4f3470e71e2e0a57
sigmotoa <si.galvismotoa@gmail.com>
2025-05-22 04:44:00 -0500

    Detail endpoint and html added

--- app/routes/home.py
+++ app/routes/home.py
@@ -17,6 +17,7 @@ async def read_home(request: Request):
 async def read_info(request:Request):
     #csv_file = "data/capacitaciones_mintic.csv"
     sesiones = pd.read_csv(csv_file)
+    sesiones["id"] = sesiones.index
     lista = sesiones.to_dict(orient="records")
     return templates.TemplateResponse("info.html",{"request":request, "sesiones":lista, "titulo":"Datos en tabla"})
 
@@ -38,9 +39,40 @@ async def submit_info(
 
     df = pd.read_csv(csv_file)
     df.loc[len(df)] = [sesion.tema, sesion.fecha, sesion.hora, sesion.nombre_mentor, sesion.link]
-    df.to_csv(csv_file, index=False)
+    df.to_csv(csv_file, index=True)
 
     return RedirectResponse(url="/info", status_code=303)
+
+
+@router.get("/detail/{id}", response_class=HTMLResponse)
+def detalle_sesion(request: Request, id: int):
+    df = pd.read_csv(csv_file)
+
+    if id < 0 or id >= len(df):
+        html_content = """
+                <html>
+                    <head>
+                        <title>sigmotoa</title>
+                    </head>
+                    <body>
+                        <h1>Look ma! HTML!</h1>
+                    </body>
+                </html>
+                """
+        return HTMLResponse(content=html_content, status_code=404)
+
+    fila = df.iloc[id]
+
+    sesion = {
+        "tema": fila["Tema"],
+        "fecha": fila["Fecha"],
+        "hora": fila["Hora"],
+        "nombre_mentor": fila["Mentor"],
+        "link": fila["Enlace"]
+    }
+
+    return templates.TemplateResponse("detail.html", {"request": request, "sesion": sesion})
+
 
 
 @router.get("/html", response_class=HTMLResponse)
